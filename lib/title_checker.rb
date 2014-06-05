1b2806c7a6315afb02226bfc10be0c010e1605fe
ichthala <amottola@constantcontact.com>
2014-06-04 20:42:13 -0400

    ...

--- lib/title_checker.rb
+++ lib/title_checker.rb
@@ -11,7 +11,6 @@ class TitleChecker
     titles_to_check = all_titles - previous_earned_titles
     earned_titles = []
 
-    binding.pry
     titles_to_check.each do |title|
       earned_titles << title if self.send("#{title.check}".to_sym)
     end
