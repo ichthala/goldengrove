a09bf6699f5d9c5bd6e7764863feaa0d465ad55c
ichthala <alice.mottola@gmail.com>
2016-03-27 21:45:45 -0400

    Fix feature spec

--- lib/twitter_service.rb
+++ lib/twitter_service.rb
@@ -1,6 +1,6 @@
 class TwitterService
 
-  def initialize(client)
+  def initialize(client = nil)
     @twitter = client || Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["CONSUMER_KEY"]
       config.consumer_secret     = ENV["CONSUMER_SECRET"]
